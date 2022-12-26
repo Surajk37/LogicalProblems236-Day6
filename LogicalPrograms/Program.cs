2cd4e5738bf9d30d9f1a47db6d212d0d2afe08ac
Surajk37 <surajkhot9993@gmail.com>
2022-12-26 15:17:08 +0530

    [Suraj] Welcome message Added in Master Branch

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome To Logical Programs\n");
+            Console.ReadLine();
+        }
+    }
+}
