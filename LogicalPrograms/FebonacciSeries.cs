76cf6fbf24e86e72f09e7f1d7eb47db029493ea9
Surajk37 <surajkhot9993@gmail.com>
2022-12-26 15:34:54 +0530

    Displayed Febonaccci Series

---
+++ LogicalPrograms/FebonacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FebonacciSeries
+    {
+        public static void Fibonacci(int n)
+        {  //To Get Fibonacci Series
+            int val1 = 0, val2 = 1, val3;
+            Console.WriteLine("The Starting Point of Febonacci Series Are {0} And {1}",val1,val2+"\n");
+            Console.Write(+val1 + " " + val2 + " ");
+            for (int i = 2; i < n; i++)
+            {
+                val3 = val1 + val2;
+                Console.Write(val3 + " ");
+                val1 = val2;
+                val2 = val3;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\tWelcome To Logical Programs\n");
+            Console.WriteLine("\tChoose the program from the below option");
+            Console.WriteLine("1] Factorial Numbers.");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter a number to get Fibonacci Series");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    FebonacciSeries.Fibonacci(num);
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter Valid Option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
