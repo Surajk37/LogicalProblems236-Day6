9e6aae4091175f87a8050e4cbc09d697f2c79501
Surajk37 <surajkhot9993@gmail.com>
2022-12-28 20:49:45 +0530

    Calculated Monthly Payment

--- LogicalPrograms/MonthlyPayment.cs
+++ LogicalPrograms/MonthlyPayment.cs
@@ -10,7 +10,7 @@ namespace LogicalPrograms
     {
         public static void GetMonthlyPayment()
         {
-            Console.WriteLine("Enter the value of principal amount: ");
+            Console.WriteLine("\nEnter the value of principal amount: ");
             double P = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter the year: ");
             double Y = Convert.ToDouble(Console.ReadLine());
@@ -20,7 +20,7 @@ namespace LogicalPrograms
             double n = 12 * Y;
             double r = R / (12 * 100);
             double payment = P * r / (1 - Math.Pow((1 + r), -n));
-            Console.WriteLine(payment + "is monthly payment");
+            Console.WriteLine(payment + " is monthly payment");
         }
     }
 }
